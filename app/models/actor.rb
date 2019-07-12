aea52babfee8e91feafb21f505a0699a5b51223d
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-12 18:38:24 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,7 +8,7 @@ class Actor < ActiveRecord::Base
 
   def list_roles
     self.characters = Character.find_or_create_by(self.full_name)
-    self.shows = Show.find_or_create_by(name: ?)
+    self.shows = Show.find_or_create_by(name)
     "#{self.characters} - #{self.shows}"
   end
 
