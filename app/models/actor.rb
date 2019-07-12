bd434087eb3faf205aa2868f784dae9d89db7524
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-12 19:01:38 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -9,7 +9,7 @@ class Actor < ActiveRecord::Base
   def list_roles
     character = self.characters.map {|col_name| col_name.name}.to_s
     show = self.shows.map {|col_name| col_name.name}
-    "#{character.to_s} - #{show}"
+    "#{character} - #{show}"
   end
 
 end
