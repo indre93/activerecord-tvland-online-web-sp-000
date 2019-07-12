b33913e8740553e1aa0c84b39d82f0b5e3a7f8f9
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-12 18:49:15 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,8 +7,8 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    character = self.characters.name.first
-    show = self.shows.name.first
+    character = self.characters.first
+    show = self.shows.first
     "#{character} - #{show}"
   end
 
